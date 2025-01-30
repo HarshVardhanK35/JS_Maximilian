5778c784785fae0870b9e9f72507c9beef769095
HarshVardhanK35 <kasavardhan2001@gmail.com>
2025-01-30 20:47:58 +0530

    functions deeper continues...

--- JS-Fun/functions.js
+++ JS-Fun/functions.js
@@ -1,3 +1,7 @@
+// functions are objects and have methods and properties
+// ex for methods: .bind, .call, .apply
+// ex for properties: .name, .length .. on functions
+
 // --- 1. default parameters: An ES6 feature
 // default values are taken only when the value is not passed
 // to skip a value, pass undefined while calling the function (eg: bookingDetails('LH230', undefined, 100)) 
@@ -135,8 +139,7 @@ const greet = function (greeting) {
         console.log(`${greeting} ${name}`)
     }
 }
-
-greet('Hey')('Jonas') // Hey Jonas
+// greet('Hey')('Jonas') // Hey Jonas
 
 // explanation:
 // greet('Hey') returns a function, which is then called with 'Jonas' as an argument
@@ -149,6 +152,194 @@ const greet1 = (greeting) => {
         console.log(`${greeting} ${name}`)
     }
 }
+// greet1('Hey')('Jonas') // Hey Jonas
 
-greet1('Hey')('Jonas') // Hey Jonas
+
+// ------------------------------------------------------------------------------------------------------------
+// --- 6. The call and apply methods
+// these methods are available on all functions because functions are objects and 
+// learn how to set the 'this' keyword manually
+// 'this' keyword works only inside a function and it will be undefined when pointed outside of function or in the global scope
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    // book: function () {}
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name: name })
+    }
+}
+
+const book = lufthansa.book
+
+// this will not work because 'this' keyword is undefined
+// book(239, "Jonas") 
+
+// as these are three function methods
+// to set the 'this' keyword manually: call, apply, and bind
+
+// --- call method
+// - this method takes three arguments:
+//  - function to call book method on to (lufthansa) and two arguments to pass to the function (book that are flightNum and name)
+// ---
+// book.call(lufthansa, 239, "Jonas")
+// console.log(lufthansa)
+
+// another example
+const swiss = {
+    airline: 'Swiss Air Lines',
+    iataCode: 'LX',
+    bookings: []
+}
+
+// --- apply method
+// - this method is similar to call method, but the only difference is that it takes an array of arguments
+// ---
+const flightData = [583, 'George']
+// book.apply(swiss, flightData) 
+// console.log(swiss)
+
+// we can use call method in place of apply method by using spread operator (...) to spread the array (if array is used to pass arguments)
+// book.call(swiss, ...flightData)
+// console.log(swiss)
+
+// summary: 
+// in call and apply methods, 1st argument is what we want this keyword point to (actually an object), 
+// and the rest of the arguments.. that we want to pass to the function for the method to work
+
+
+// ------------------------------------------------------------------------------------------------------------
+// --- 7. The bind method
+
+// case(1):
+// - this also allows us to manually set the 'this' keyword, but it does not immediately call the function 
+//  - instead, it returns a new function where 'this' keyword is bound
+// 
+
+const euroWings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: []
+}
+
+// book.call(euroWings, 23, 'Sarah') // this immediately calls the function
+const bookEW = book.bind(euroWings) // this does not immediately call the function, but returns a new function
+// bookEW(23, 'Sarah')
+
+// ... partial applications: 
+// - part of arguments of the original function are set, while using .bind to set this manually!
+// const bookEW23 = book.bind(euroWings, 23)
+// bookEW('Harsha')
+
+// ... case(2): 
+// when we use objects together with event listeners
+
+
+// ------------------------------------------------------------------------------------------------------------
+// challenge-1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. 
+    This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. 
+  Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+
+2. Call this method whenever the user clicks the "Answer poll" button.
+
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), 
+    which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). 
+This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. 
+Do NOT put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+    
+    question: "what is your favourite programming language?",
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    // this generates [0, 0, 0, 0]
+
+    answers: new Array(4).fill(0),
+
+    // Get the answer
+    registerNewAns: function() {
+        const option = +prompt(`${this.question}\n${this.options.join('\n')} (Write option number)`)
+        typeof option === 'number' && option < this.options.length && this.answers[option]++
+
+        this.displayResults()
+        this.displayResults('string')
+    },
+
+    // display results
+    displayResults: function(type = 'array') {
+        if (type.toLowerCase() === 'string') {
+            console.log(`Poll results are: ${this.answers.join(', ')}`)
+        }
+        else {
+            console.log(this.answers)
+        }
+    }
+};
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAns.bind(poll))
+
+const anObject = {
+    answers: [5, 2, 3]
+}
+// poll.displayResults.call(anObject)
+// poll.displayResults.call(anObject, 'string')
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string')
+
+
+// ------------------------------------------------------------------------------------------------------------
+// --- 8. Immediately Invoked Functions
+// in JS we just write a function and call it immediately and then after that, we do not call it again (run once functions)
+
+// normal function
+// const runOnce = function () {
+//     console.log('This will never run again')
+// }
+// runOnce()
+
+// IIFE
+// 1.
+(function () {
+    console.log('This will never run again')
+})()
+
+// 2.
+(() => {
+    console.log('This will also never run again');
+})()
+
+// (function () {
+//     console.log('This will also never run again')            } this is a function expression wrapped in parentheses
+// }) ()                                                 --- called immediately here
 
